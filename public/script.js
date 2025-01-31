6a732e69c54eb2edaf94e03dd6660567c6ce060c
sonali <b2028fdrahman@pshs.edu.ph>
2025-01-31 08:54:36 +0800

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,36 @@
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+function function1 () {
+  window.location.href = "page1.html";
+}
+function function2 () {
+  window.location.href = "page2.html";
+}
+function function3 () {
+  window.location.href = "index.html";
+}
+function function4 () {
+  window.location.href = "page3.html";
+}
+function function5 () {
+  window.location.href = "page4.html";
+}
+function addA () {
+  a++;
+  return a;
+}
+function addB () {
+  b++;
+  return b;
+}
+function addC () {
+  c++;
+  return c;
+  console.log(c)
+}
+function addD () {
+  d++;
+  return d;
+}
